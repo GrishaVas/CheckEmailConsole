c8cc2bde2dbc30108057821609a6cadc574dd0f0
Григорий Васильчук <grisha.vasilchuk.200201@gmail.com>
2024-10-07 18:46:00 +0300

    Add project files.

---
+++ CheckEmailConsole/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using DnsClient;
+
+
+namespace CheckEmailConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                const string inputTextFile = "input.txt";
+                var recipients = new List<string>();
+
+
+                using (var streamReader = new StreamReader(inputTextFile))
+                {
+                    var line = "";
+
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        recipients.Add(line);
+                    }
+                }
+
+                foreach (var recipient in recipients)
+                {
+                    var success = false;
+                    var response = checkEmail(recipient, out success);
+
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write(" email: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(recipient);
+                    Console.ForegroundColor = success ? ConsoleColor.Green : ConsoleColor.Red;
+                    Console.Write(" repsonse: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(response);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine();
+                    Console.WriteLine("///////////////////////////////////////////////////////////////////////////////");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+        }
+
+        static string checkEmail(string email, out bool success)
+        {
+            var startDomainIndex = email.IndexOf('@') + 1;
+            var domain = email.Substring(startDomainIndex, email.Length - startDomainIndex);
+
+            var iPAddres = IPAddress.Parse("8.8.8.8");
+            var lookup = new LookupClient(new LookupClientOptions(iPAddres));
+
+            var result = lookup.Query(domain, QueryType.ANY);
+            var anyRecords = result.Answers.ToList();
+
+            if (anyRecords.Count <= 0)
+            {
+                success = false;
+
+                return "dns have no any-record";
+            }
+
+            result = lookup.Query(domain, QueryType.MX);
+            var mxRecords = result.Answers.ToList();
+
+            if (mxRecords.Count <= 0)
+            {
+                success = false;
+
+                return "dns have no mx-record";
+            }
+
+            const string senderEmail = "";
+            const string senderPass = "";
+
+            var response = "";
+
+            using (var stmpGmailServer = new SmtpGmailServer())
+            {
+                try
+                {
+                    stmpGmailServer.ExecuteAUTHLOGIN(senderEmail, senderPass);
+                    stmpGmailServer.ExecuteEHLO();
+                    stmpGmailServer.ExecuteMAILFROM(senderEmail);
+                    stmpGmailServer.ExecuteRCPTTO(email);
+                    response = stmpGmailServer.ExecuteDATA("Test", "It works");
+                    stmpGmailServer.ExecuteQuit();
+                }
+                catch (Exception ex)
+                {
+                    success = false;
+
+                    return ex.Message;
+                }
+
+            }
+
+            success = true;
+
+            return response;
+        }
+    }
+}
---
+++ CheckEmailConsole/SmtpGmailServer.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CheckEmailConsole
+{
+    public class SmtpGmailServer : IDisposable
+    {
+        private const string hostName = "smtp.gmail.com";
+        private const int hostPort = 587;
+        private TcpClient _tcpClient;
+        private SslStream _sslStream;
+
+        public SmtpGmailServer()
+        {
+            var buffer = new byte[1024];
+            var str = "";
+
+            _tcpClient = new TcpClient();
+            _tcpClient.Connect(hostName, hostPort);
+            Console.WriteLine(" Smtp server:");
+            _tcpClient.Client.Receive(buffer);
+            str = Encoding.UTF8.GetString(buffer);
+            checkResponse(str, '2');
+            executeCommand($"EHLO {hostName}", '2');
+            executeCommand("STARTTLS", '2');
+
+            _sslStream = new SslStream(_tcpClient.GetStream());
+
+            _sslStream.AuthenticateAsClient(hostName);
+
+            var auth = _sslStream.IsAuthenticated;
+            Console.WriteLine("     IsAuthenticated:" + auth);
+            if (!auth)
+            {
+                throw new Exception("Authentication failed");
+            }
+        }
+
+        public string ExecuteVRFY(string mail)
+        {
+            return executeCommand($"VRFY {mail}", '2', true);
+        }
+        public string ExecuteEHLO()
+        {
+            return executeCommand("EHLO gmail.com", '2', true);
+        }
+
+        public string ExecuteAUTHLOGIN(string senderEmail, string senderPassword)
+        {
+            var str = executeCommand("AUTH LOGIN", '3', true);
+
+            str = sendString(senderEmail, true);
+            checkResponse(str, '3');
+            str = sendString(senderPassword, true);
+            checkResponse(str, '2');
+
+            return str;
+        }
+
+        public string ExecuteMAILFROM(string senderEmail)
+        {
+            var str = executeCommand($"MAIL FROM: <{senderEmail}>", '2', true);
+
+            return str;
+        }
+
+        public string ExecuteRCPTTO(string receiverEmail)
+        {
+            var str = executeCommand($"RCPT TO: <{receiverEmail}>", '2', true);
+
+            return str;
+        }
+
+        public string ExecuteDATA(string subject, string body)
+        {
+            var str = executeCommand($"DATA", '3', true);
+
+            sendStringWithoutResponse($"Subject: {subject}");
+            sendStringWithoutResponse($"{body}");
+            str = sendString(".");
+            checkResponse(str, '2');
+
+            return str;
+        }
+
+        public string ExecuteQuit()
+        {
+            var str = executeCommand($"quit", '2', true);
+
+            return str;
+        }
+
+        private string sendString(string data, bool convertToBase64 = false)
+        {
+            var buffer = new byte[1024];
+            var str = "";
+
+            data = convertToBase64 ? Convert.ToBase64String(Encoding.UTF8.GetBytes(data)) : data;
+            _sslStream.Write(Encoding.UTF8.GetBytes($"{data}\r\n"));
+            _sslStream.Read(buffer, 0, buffer.Length);
+            str = Encoding.UTF8.GetString(buffer);
+
+
+            Console.WriteLine("     " + data);
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("     " + str.Substring(0, str.IndexOf('\0') + 1));
+            Console.ForegroundColor = ConsoleColor.White;
+
+            return str.Substring(0, str.IndexOf('\0') + 1);
+        }
+        private void sendStringWithoutResponse(string data, bool convertToBase64 = false)
+        {
+            data = convertToBase64 ? Convert.ToBase64String(Encoding.UTF8.GetBytes(data)) : data;
+            _sslStream.Write(Encoding.UTF8.GetBytes($"{data}\r\n"));
+            Console.WriteLine("     " + data);
+        }
+
+        private string executeCommand(string command, char firstNumberOfCode, bool withSsl = false)
+        {
+            var buffer = new byte[1024];
+            var str = "";
+
+            if (withSsl)
+            {
+                _sslStream.Write(Encoding.UTF8.GetBytes($"{command}\r\n"));
+                _sslStream.Read(buffer, 0, buffer.Length);
+                str = Encoding.UTF8.GetString(buffer);
+            }
+            else
+            {
+                _tcpClient.Client.Send(Encoding.UTF8.GetBytes($"{command}\r\n"));
+                _tcpClient.Client.Receive(buffer);
+                str = Encoding.UTF8.GetString(buffer);
+            }
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("     " + command);
+            checkResponse(str, firstNumberOfCode);
+
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("     " + str.Substring(0, str.IndexOf('\0') + 1).Replace("\n", "\n     "));
+            Console.ForegroundColor = ConsoleColor.White;
+
+            return str.Substring(0, str.IndexOf('\0') + 1);
+        }
+
+        private string[] checkResponse(string str, char firstNumberOfCode)
+        {
+            var responseLines = str.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (responseLines[0][0] != firstNumberOfCode)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("     " + str.Substring(0, str.IndexOf('\0') + 1));
+                Console.ForegroundColor = ConsoleColor.White;
+                throw new Exception(str);
+            }
+
+            return responseLines;
+        }
+
+        public void Dispose()
+        {
+            ExecuteQuit();
+            _tcpClient.Dispose();
+            _sslStream.Dispose();
+
+            GC.SuppressFinalize(this);
+        }
+        ~SmtpGmailServer()
+        {
+            Dispose();
+        }
+    }
+}
